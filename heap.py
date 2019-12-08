01cfda0360d5e9922d055bcebf57cef108f40101
Makoto Ishihara <makoto.ishihara.jp@gmail.com>
2019-12-08 18:49:52 +0900

    some modification

--- heap.py
+++ heap.py
@@ -1,16 +1,21 @@
 # coding: utf-8
 from random import randrange
-from heapmax import heapMax
-HEAP_NUM = 7
+
+HEAP_NUM = 9
 heap = [0] * (HEAP_NUM + 1)
+
 def insert(val, heap, counter):
+    """
+    1. 
+    """
     i=counter
     while ((i != 1) and (heap[i//2] < val)):
         heap[i] = heap[i//2]
         i = i//2
     heap[i] = val
+
 for i in range (1, (HEAP_NUM + 1)):
     val = randrange(100)
     insert(val, heap, i)
-    print("insert[%d]: %d" %(i, val))
+    print("insert[[{0:d}]: {1:d}".format(i, val))
     
