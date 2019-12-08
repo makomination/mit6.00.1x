5f40aef66cb8098732fbb06af0bda76472d6a02e
Makoto Ishihara <makoto.ishihara.jp@gmail.com>
2019-12-08 18:52:18 +0900

    some lint fix

--- untitled2.py
+++ untitled2.py
@@ -7,12 +7,12 @@ Created on Mon Nov 12 13:01:24 2018
 """
 
 epsilon = 0.0001
-y= 24.0
+y = 24.0
 guess = y/2.0
 numGuesses = 0
 
 while abs(guess * guess - y) >= epsilon:
     numGuesses += 1
-    guess = guess - (((guess ** 2) - y) / (2 * guess) )
+    guess = guess - (((guess ** 2) - y) / (2 * guess))
 print('numGuesses = ' + str(numGuesses))
 print('Square root of ' + str(y) + ' is about ' + str(guess))
